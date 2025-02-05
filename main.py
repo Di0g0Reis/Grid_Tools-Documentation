337990ce9cc9527e9a29779dfd8b39af3fe99353
Diogo Reis <diogo.a.reis@inesctec.pt>
2025-02-05 14:45:28 +0000

    main file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
-
+from network import Network
 
 
 
 if __name__ == "__main__":
 
     case_study = os.path.join('data', 'CS1')
-    network_filename = os.path.join(case_study, "case33_1_2045.json")
+    network_filename = os.path.join(case_study, "case33.json")
+
+    network = Network()
+    network.load_topology(network_filename)
+    print()
