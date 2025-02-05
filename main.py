3521393778f9760fa303d5cb41bdb56edabd3d18
Diogo Reis <diogo.a.reis@inesctec.pt>
2025-02-05 14:45:28 +0000

    main file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello, World!')
+import os
+from network import Network
+
+
+
+if __name__ == "__main__":
+
+    case_study = os.path.join('data', 'CS1')
+    network_filename = os.path.join(case_study, "case33.json")
+
+    network = Network()
+    network.load_topology(network_filename)
+    print()
