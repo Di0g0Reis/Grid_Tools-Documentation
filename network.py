c894b236733d4f99c0ae3362cb8ecbb91ad293ef
Diogo Reis <diogo.a.reis@inesctec.pt>
2025-02-05 17:41:15 +0000

    network file updated

--- network.py
+++ network.py
@@ -6,6 +6,10 @@ from branch import Branch
 from generator import Generator
 from capacitor_bank import CapacitorBank
 from energy_storage import EnergyStorage
+import pandas as pd
+from helper_functions import *
+
+
 
 class Network:
 
@@ -33,6 +37,21 @@ class Network:
     def load_topology(self, filename):
         _load_topology(self, filename)
 
+    def construct_sheet_names(self, season, scenario):
+        sheet_names = _construct_sheet_names(season, scenario)
+        return sheet_names
+
+    def load_case(self, excel_file, sheet_names):
+        data = _load_case(self, excel_file, sheet_names)
+        return data
+
+    def read_market_data(self, market_data_file, sheet_names, scenario):
+        _read_market_data(self, market_data_file, sheet_names, scenario)
+
+
+#=======================================================
+#           Read JSON data, Topology
+#=======================================================
 
 def _load_topology(network, filename):
 
@@ -152,3 +171,216 @@ def _load_topology(network, filename):
             network.energy_storages.append(es_instance)
 
     return network
+
+
+#=======================================================
+#           Read Excel data, Scenarios
+#=======================================================
+
+def _construct_sheet_names(season, scenario):
+    """
+    Construct the sheet names based on the season and scenario.
+    """
+    prefix = {
+        'Winter': 'Winter',
+        'Spring': 'Spring',
+        'Summer': 'Summer',
+        'Fall': 'Fall'
+    }
+    return {
+        'Pc': f'Pc, {prefix[season]}, S{scenario}',
+        'Qc': f'Qc, {prefix[season]}, S{scenario}',
+        'Pg': f'Pg, {prefix[season]}, S{scenario}',
+        'Qg': f'Qg, {prefix[season]}, S{scenario}',
+        'UpFlex': f'UpFlex, {prefix[season]}',
+        'DownFlex': f'DownFlex, {prefix[season]}',
+        'GenStatus': f'GenStatus, {prefix[season]}',
+        'Cp': f'Cp, {prefix[season]}',
+        'Flex': f'Flex, {prefix[season]}'
+
+    }
+
+def _load_case(network, excel_file, sheet_names):
+
+    network.operational_data_file = excel_file
+
+    data = {
+        'consumption': {
+            'pc': dict(), 'qc': dict()
+        },
+        'flexibility': {
+            'upward': dict(),
+            'downward': dict()
+        },
+        'generation': {
+            'pg': dict(), 'qg': dict(), 'status': list()
+        }
+    }
+
+    # Scenario information
+    num_gen_cons_scenarios, prob_gen_cons_scenarios = _get_operational_scenario_info_from_excel_file(excel_file, 'Main')
+    network.prob_operation_scenarios = prob_gen_cons_scenarios
+
+    # Consumption and Generation data -- by scenario
+    for i in range(len(network.prob_operation_scenarios)):
+
+        # Consumption per scenario (active, reactive power)
+        pc_scenario = _get_consumption_flexibility_data_from_excel_file(excel_file, sheet_names["Pc"])
+        qc_scenario = _get_consumption_flexibility_data_from_excel_file(excel_file, sheet_names["Qc"])
+        if not pc_scenario:
+            print(f'[ERROR] Network {network.name}, {network.year}, {network.day}. No active power consumption data provided for scenario {i + 1}. Exiting...')
+        if not qc_scenario:
+            print(f'[ERROR] Network {network.name}, {network.year}, {network.day}. No reactive power consumption data provided for scenario {i + 1}. Exiting...')
+        data['consumption']['pc'][i] = pc_scenario
+        data['consumption']['qc'][i] = qc_scenario
+
+        # Generation per scenario (active, reactive power)
+        num_renewable_gens = network.get_num_renewable_gens()
+        if num_renewable_gens > 0:
+            pg_scenario = _get_generation_data_from_excel_file(excel_file, sheet_names["Pg"])
+            qg_scenario = _get_generation_data_from_excel_file(excel_file, sheet_names["Qg"])
+            if not pg_scenario:
+                print(f'[ERROR] Network {network.name}, {network.year}, {network.day}. No active power generation data provided for scenario {i + 1}. Exiting...')
+            if not qg_scenario:
+                print(f'[ERROR] Network {network.name}, {network.year}, {network.day}. No reactive power generation data provided for scenario {i + 1}. Exiting...')
+            data['generation']['pg'][i] = pg_scenario
+            data['generation']['qg'][i] = qg_scenario
+
+    # Generators status. Note: common to all scenarios
+    data['generation']['status'] = _get_generator_status_from_excel_file(excel_file, sheet_names["GenStatus"])
+
+    # Flexibility data
+    flex_up_p = _get_consumption_flexibility_data_from_excel_file(excel_file, sheet_names["UpFlex"])
+    if not flex_up_p:
+        for load in network.loads:
+            flex_up_p[load.load_id] = [0.0 for _ in range(network.num_instants)]
+    data['flexibility']['upward'] = flex_up_p
+
+    flex_down_p = _get_consumption_flexibility_data_from_excel_file(excel_file, sheet_names["DownFlex"])
+    if not flex_down_p:
+        for load in network.loads:
+            flex_down_p[load.load_id] = [0.0 for _ in range(network.num_instants)]
+    data['flexibility']['downward'] = flex_down_p
+
+    return data
+
+def _get_operational_scenario_info_from_excel_file(filename, sheet_name):
+
+    num_scenarios = 0
+    prob_scenarios = list()
+
+    try:
+        df = pd.read_excel(filename, sheet_name=sheet_name, header=None)
+        if is_int(df.iloc[0, 1]):
+            num_scenarios = int(df.iloc[0, 1])
+        for i in range(num_scenarios):
+            if is_number(df.iloc[0, i+2]):
+                prob_scenarios.append(float(df.iloc[0, i+2]))
+    except:
+        print('[ERROR] Workbook {}. Sheet {} does not exist.'.format(filename, sheet_name))
+        exit(1)
+
+    if num_scenarios != len(prob_scenarios):
+        print('[WARNING] Workbook {}. Data file. Number of scenarios different from the probability vector!'.format(filename))
+
+    if round(sum(prob_scenarios), 2) != 1.00:
+        print('[ERROR] Workbook {}. Probability of scenarios does not add up to 100%.'.format(filename))
+
+    return num_scenarios, prob_scenarios
+
+def _get_consumption_flexibility_data_from_excel_file(filename, sheet_name):
+
+    try:
+        data = pd.read_excel(filename, sheet_name=sheet_name)
+        num_rows, num_cols = data.shape
+        processed_data = dict()
+        for i in range(num_rows):
+            node_id = data.iloc[i, 0]
+            processed_data[node_id] = [0.0 for _ in range(num_cols - 1)]
+        for node_id in processed_data:
+            node_values = [0.0 for _ in range(num_cols - 1)]
+            for i in range(0, num_rows):
+                aux_node_id = data.iloc[i, 0]
+                if aux_node_id == node_id:
+                    for j in range(0, num_cols - 1):
+                        node_values[j] += data.iloc[i, j + 1]
+            processed_data[node_id] = node_values
+    except:
+        print(f'[WARNING] Workbook {filename}. Sheet {sheet_name} does not exist.')
+        processed_data = {}
+
+    return processed_data
+
+def _get_generation_data_from_excel_file(filename, sheet_name):
+
+    try:
+        data = pd.read_excel(filename, sheet_name=sheet_name)
+        num_rows, num_cols = data.shape
+        processed_data = dict()
+        for i in range(num_rows):
+            gen_id = data.iloc[i, 0]
+            processed_data[gen_id] = [0.0 for _ in range(num_cols - 1)]
+        for gen_id in processed_data:
+            processed_data_gen = [0.0 for _ in range(num_cols - 1)]
+            for i in range(0, num_rows):
+                aux_node_id = data.iloc[i, 0]
+                if aux_node_id == gen_id:
+                    for j in range(0, num_cols - 1):
+                        processed_data_gen[j] += data.iloc[i, j + 1]
+            processed_data[gen_id] = processed_data_gen
+    except:
+        print(f'[WARNING] Workbook {filename}. Sheet {sheet_name} does not exist.')
+        processed_data = {}
+
+    return processed_data
+
+def _get_generator_status_from_excel_file(filename, sheet_name):
+
+    try:
+        data = pd.read_excel(filename, sheet_name=sheet_name)
+        num_rows, num_cols = data.shape
+        status_values = dict()
+        for i in range(num_rows):
+            gen_id = data.iloc[i, 0]
+            status_values[gen_id] = list()
+            for j in range(0, num_cols - 1):
+                status_values[gen_id].append(bool(data.iloc[i, j + 1]))
+    except:
+        print(f'[WARNING] Workbook {filename}. Sheet {sheet_name} does not exist.')
+        status_values = list()
+
+    return status_values
+
+
+#=======================================================
+#           Read Market data
+#=======================================================
+
+def _read_market_data(network, file_path, sheet_names, scenario):
+    # Initialize variables to hold market data separately
+    Cp, Flex, prob_market_scenarios = None, None, None
+
+    # Read the specified sheet for the given season
+    try:
+        # Load the entire Cp sheet and select the specific row for the scenario
+        Cp_data = pd.read_excel(file_path, sheet_name=sheet_names['Cp'])
+        Cp = Cp_data.iloc[scenario - 1]  # Adjusting for zero-based index
+        network.cost_energy_p.append(Cp)
+        print(f"Successfully read Cp data from {sheet_names['Cp']} for scenario {scenario}")
+
+        # Load the entire Flex sheet and select the specific row for the scenario
+        Flex_data = pd.read_excel(file_path, sheet_name=sheet_names['Flex'])
+        Flex = Flex_data.iloc[scenario - 1]  # Adjusting for zero-based index
+        network.cost_flex.append(Flex)
+        print(f"Successfully read Flex data from {sheet_names['Flex']} for scenario {scenario}")
+
+        # Load the Probability of market (price) scenarios
+        prob_data = pd.read_excel(file_path, sheet_name='Scenarios', header=None)
+        prob_market_scenarios = prob_data.iloc[0, 2:].tolist()
+        network.prob_market_scenarios.append(prob_market_scenarios)
+        print(f"Successfully read Prob. data")
+
+    except Exception as e:
+        print(f"Error reading {sheet_names['Cp']} or , {sheet_names['Flex']}, or Prob: {e}")
+
+    return network.cost_energy_p, network.cost_flex, network.prob_market_scenarios
