6296891b89bf9c29e0385746033a463536d692e4
micaelsimoes <micaelsimoes@gmail.com>
2025-02-05 11:18:43 +0000

    Bck.

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import os
-
-
-
-
-if __name__ == "__main__":
-
-    case_study = os.path.join('data', 'CS1')
-    network_filename = os.path.join(case_study, "case33_1_2045.json")
+print('Hello, World!')
