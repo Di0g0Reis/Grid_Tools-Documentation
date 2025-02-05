1749575e31a516856e4847bcebb06522a4d8472a
Diogo Reis <diogo.a.reis@inesctec.pt>
2025-02-05 17:41:02 +0000

    main file updated

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ if __name__ == "__main__":
 
     case_study = os.path.join('data', 'CS1')
     network_filename = os.path.join(case_study, "case33.json")
+    network_excel_file = os.path.join(case_study, "case33.xlsx")
+    market_data_file = os.path.join(case_study, "CS7_market_data.xlsx")
+
+    season = "Summer"  # Change this as needed
+    scenario = 2  # Change this as needed
 
     network = Network()
     network.load_topology(network_filename)
+    sheet_names = network.construct_sheet_names(season, scenario)
+    network.load_case(network_excel_file, sheet_names)
+    network.read_market_data(market_data_file, sheet_names, scenario)
     print()
