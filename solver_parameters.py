1f8d89860ee3d0767d9e2a895603a35b32dc4a94
micaelsimoes <micaelsimoes@gmail.com>
2025-02-07 11:05:42 +0000

    solver_parameters.py. Upload.

---
+++ solver_parameters.py
@@ -0,0 +1,15 @@
+import os
+
+
+# ============================================================================================
+#   Class SolverParameters
+# ============================================================================================
+class SolverParameters:
+
+    def __init__(self):
+        self.solver = 'ipopt'
+        self.linear_solver = 'ma57'
+        self.nlp_solver = 'ipopt'
+        self.solver_path = os.environ['IPOPTDIR']
+        self.solver_tol = 1e-6
+        self.verbose = False
